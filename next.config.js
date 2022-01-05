bd99290aa21cdecfb351eb7f97a5f79bffecbebb
devgeetech <devgeetech@gmail.com>
2022-01-05 21:06:29 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  rewrites: () => [STUDIO_REWRITE],
+};
+
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     colors: {
       white: colors.white,
